673712af4310f834d836753d2e5722bbbbb562d4
BoYao Zhang <rZhBoYao@gmail.com>
2020-02-17 23:26:25 +0800

    Update searching.hpp

--- searching/searching.hpp
+++ searching/searching.hpp
@@ -15,6 +15,7 @@ constexpr BidirectionalIterator linear_search(BidirectionalIterator first, Bidir
 // requires a pair of iterators for a half open sorted sequence, and a target value
 // returns the iterator for the target value (if any),
 //   or the past the end iterator for the given sequence otherwise
+// the code is more readable with RandomAccessIterator
 template<typename RandomAccessIterator, typename T>
 constexpr RandomAccessIterator binary_find(RandomAccessIterator first, RandomAccessIterator last, const T& value) {
   auto past_the_end = last;
