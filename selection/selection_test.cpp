e1a4ba7da562eab33661baa7f572247c496f75df
BoYao Zhang <rZhBoYao@gmail.com>
2020-02-18 22:29:44 +0800

    implement find_min_max and randomized_select

--- selection/selection_test.cpp
+++ selection/selection_test.cpp
@@ -2,10 +2,20 @@
 #include <iostream>
 
 int main() {
-  std::vector<int> v = {7, 23, 11, 13, 29, 19, 21, 5, 43, 9, 37, 15, 25, 31};
-  std::cout << v << '\n';
+  std::vector<int> v = {7, 23, 11, 13, 29, 19, 21, 5, 43, 9, 37, 15, 25, 31, 87};
+  auto [min, max] = find_min_max(v);
+
+  std::cout << "min: " << min << '\n'
+            << "max: " << max << std::endl;
+
+  // start from 0
+  for (size_t i = 0; i < v.size(); ++i)
+    std::cout << i << "th: " << randomized_select(v, 0, v.size() - 1, i) << '\n';
+
   sort(v);
-  std::cout << v << '\n';
+  std::cout << "sorted:\n"
+            << v << '\n';
   sort(v, std::greater<int>());
-  std::cout << v << '\n';
+  std::cout << "sorted:\n"
+            << v << '\n';
 }
---
+++ sorting/quick_sort.hpp
@@ -0,0 +1,7 @@
+template<typename Iterator, typename Compare>
+Iterator Lomuto_partition(Iterator first, Iterator last, Compare comp) {
+}
+
+template<typename Iterator, typename Compare>
+Iterator Hoare_partition(Iterator first, Iterator last, Compare comp) {
+}
