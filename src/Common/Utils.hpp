d67a3dda13e273dfffd0235f7d87cf3698d812bd
BoYao Zhang <rZhBoYao@gmail.com>
2020-05-24 03:46:57 +0800

    [KMP][NFC] Replace unicode pi symbol with pi

--- src/Common/Utils.hpp
+++ src/Common/Utils.hpp
@@ -29,7 +29,7 @@ public:
   int operator()() { return dist(re); }
 
 private:
-  std::random_device        r;
+  std::random_device              r;
   std::default_random_engine      re;
   std::uniform_int_distribution<> dist;
 };
--- src/StringMatching/KMP.cpp
+++ src/StringMatching/KMP.cpp
@@ -5,31 +5,31 @@
 // more like a failure function
 std::vector<int> computePrefixFunction(const std::string& p) {
   const auto       m = p.length();
-  std::vector<int> 𝛑(m);
-  𝛑[0]   = -1;
+  std::vector<int> pi(m);
+  pi[0]  = -1;
   auto k = -1;
   for (size_t q = 1; q < m; ++q) {
     while (k > -1 && p[k + 1] != p[q]) {
-      k = 𝛑[k];
+      k = pi[k];
     }
     if (p[k + 1] == p[q]) {
       ++k;
     }
-    𝛑[q] = k;
+    pi[q] = k;
   }
-  return 𝛑;
+  return pi;
 }
 
 std::vector<size_t> kmpMatcher(const std::string& t, const std::string& p) {
-  const auto          n = t.length();
-  const auto          m = p.length();
-  auto                𝛑 = computePrefixFunction(p);
-  auto                q = -1; // match to what position in pattern
+  const auto          n  = t.length();
+  const auto          m  = p.length();
+  auto                pi = computePrefixFunction(p);
+  auto                q  = -1; // match to what position in pattern
   std::vector<size_t> matched;
 
   for (size_t i = 0; i < n; ++i) {
     while (q > -1 && p[q + 1] != t[i]) {
-      q = 𝛑[q];
+      q = pi[q];
     }
 
     if (p[q + 1] == t[i]) {
@@ -38,7 +38,7 @@ std::vector<size_t> kmpMatcher(const std::string& t, const std::string& p) {
 
     if (q == static_cast<int>(m) - 1) {
       matched.push_back(i - q);
-      q = 𝛑[q];
+      q = pi[q];
     }
   }
   return matched;
